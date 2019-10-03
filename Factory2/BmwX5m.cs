4220316fcfd4b16921585be6037c276a20cc8127
prunce <cprunty@greatland.com>
2019-10-03 09:36:23 -0500

    adding concrete car classes

---
+++ Factory2/BmwX5m.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factory2
+{
+    public class BmwX5m : IAuto
+    {
+        public void TurnOff()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TurnOn()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Factory2/LincolnAviator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factory2
+{
+    public class LincolnAviator : IAuto
+    {
+        public void TurnOff()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TurnOn()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
